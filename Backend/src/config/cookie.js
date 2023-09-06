d1caba132b6ca72e4ba11dac9b5954bf6aef37f4
prathamsm7 <mungekarp8@gmail.com>
2023-09-06 22:50:06 +0530

    secure cookie bug fixed

--- Backend/src/config/cookie.js
+++ Backend/src/config/cookie.js
@@ -2,6 +2,7 @@ const cookie = (user = {}, token, res) => {
   const options = {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    secure: true,
     sameSite: "none",
   };
   res.cookie("token", token, options).json(user);
